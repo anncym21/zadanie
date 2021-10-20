ccf2c2a1164e88afcffcf923e97691512307161a
anncym21 <visualstudent@zschie.edu.pl>
2021-10-20 12:35:49 +0200

    komentarz

--- zad/Form1.cs
+++ zad/Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace zad
 {
     public partial class Form1 : Form
-    {//
+    {///
         string imie_wartosc = "";
         string nazwisko_wartosc = "";
         DateTime data_i_czas;
